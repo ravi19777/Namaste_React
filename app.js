10b08f91728b0af78f2fabaf3237763e5b23e0df
Ravi <raviraushanrai@gmail.com>
2023-09-26 12:48:44 +0530

    episode-2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from 'react';  // importing "React" from 'react' module
+
+import ReactDOM from 'react-dom/client' // import "ReactDOM" from 'react-dom' module ;
+
 // <!-- react element creation -->
 const heading1 = React.createElement('h3',{},"This is the children of the react element!!");
 
